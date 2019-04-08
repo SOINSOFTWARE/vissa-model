d9eb0b7530b9715087b82470cba60b22a2317d46
Carlos Rodriguez <carlosandres.rodriguezpatino@gmail.com>
2019-04-07 22:26:24 -0500

    Validar  list lots

--- src/main/java/com/soinsoftware/vissa/bll/LotBll.java
+++ src/main/java/com/soinsoftware/vissa/bll/LotBll.java
@@ -48,6 +48,11 @@ public class LotBll extends AbstractBll<Lot, BigInteger> {
 		List<Lot> lots = ((LotDao) dao).select(expirationDate);
 		return lots;
 	}
+	
+	public  List<Lot>  selecLotWithStock(Product product) {
+		List<Lot> lots = ((LotDao) dao).selectWithStock(product);
+		return lots;
+	}
 
 	public Lot getLastLotByProduct(Product product) {
 		Lot lot = null;
@@ -62,17 +67,12 @@ public class LotBll extends AbstractBll<Lot, BigInteger> {
 		return lot;
 	}
 
-	public Lot getLastLotWithStockByProduct(Product product) {
+	public Lot getOlderLotWithStockByProduct(Product product) {
 		Lot lot = null;
 		List<Lot> lots = ((LotDao) dao).selectWithStock(product);
 		lots = lots.stream().sorted(Comparator.comparing(Lot::getCreationDate).reversed()).collect(Collectors.toList());
 		lot = lots.get(lots.size() - 1);
-		/*
-		 * List<Lot> lotsByCode = lots.stream().sorted(Comparator.comparing(lotTmp -> {
-		 * return Integer.parseInt(lotTmp.getCode()); })).collect(Collectors.toList());
-		 * int size = lotsByCode.size(); if (size > 0) { lot = lotsByCode.get(size - 1);
-		 * }
-		 */
+
 		return lot;
 	}
 
